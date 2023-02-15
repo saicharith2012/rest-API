dd9797070c1e8fa753e8cb274c9a650e3bd28504
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-02-15 06:19:47 +0530

    setup routes and middleware using express app

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 //entry point of our application
 
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+// express app setup
+const app = express();
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost/ourdata");
+// ourdata is the name of the model
+mongoose.Promise = global.Promise;
+
+// middleware
+app.use(express.static("public")); //to serve static files
+// EXPRESS PARSER to parse the data sent or recieved from the database.
+app.use(express.json());
+
+// routes
+app.use("/api", require("./routes/api"));
+
+// middleware to handle errors
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
 
 //app listening to requests at port 4000
-app.listen(process.env.port || 4000, ()=>{
-    console.log('listening for the requests now.')
-}) //checking for any port no. defined via env variables
+app.listen(process.env.port || 4000, () => {
+  console.log("listening for the requests now.");
+}); //checking for any port no. defined via env variables
 
-
-app.get('/api', (req, res)=>{
-    res.send("Its Working!!")
-})
+// app.get('/api', (req, res)=>{
+//     res.send("Its Working!!")
+// })
